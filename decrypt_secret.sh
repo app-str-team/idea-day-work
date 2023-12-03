7cbd9ed1c7f3058bc0b67d92e8507402e6f5ec2d
“Vivekvardhan <vivekvardhan.jilkara@communitybrands.com>
2023-12-03 19:22:49 +0530

    fixing path issues

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -5,5 +5,5 @@ gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output ./src/persistence/app-str-team-6577bb093acd.json ./src/persistence/app-str-team-6577bb093acd.json.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output ./src/persistence/firebase_config.json ./src/persistence/firebase_config/firebase_config.json.gpg
+--output ./src/persistence/firebase_config.json ./src/persistence/firebase_config.json.gpg
 
