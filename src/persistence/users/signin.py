52d08da50f90667a595518bc49f5e01b6413d644
“Vivekvardhan <vivekvardhan.jilkara@communitybrands.com>
2023-12-04 21:24:28 +0530

    further updates

--- src/persistence/users/signin.py
+++ src/persistence/users/signin.py
@@ -3,7 +3,6 @@ import requests
 from .. import firestoreconnection
 
 
-
 def process_signin(email: str, password: str, return_secure_token: bool = True) ->dict:
     payload = json.dumps({
         "email": email,
@@ -15,6 +14,16 @@ def process_signin(email: str, password: str, return_secure_token: bool = True) 
                       params={"key": firestoreconnection.CONFIG['FIREBASE_WEB_API_KEY']},
                       data=payload)
     
-    return r.json()
+    resp_fields = r.json()
+    claims = firestoreconnection.auth.verify_id_token(resp_fields['idToken'])
+    if 'Judge' in claims:
+        if claims['Judge'] is True:
+            resp_fields['userRole'] = 'Judge'
+        else:
+            resp_fields['userRole'] = 'noRole' 
+    else:
+        resp_fields['userRole'] = 'noRole'
+    return resp_fields
+
 
     
